7a74880060d3dcc49854a2d066233bb89c772140
Martynas Sudintas <martynas.sudintas@ongr.io>
2015-09-02 15:56:20 +0300

    Updated python.sh file with -qq instead outputing to null

--- vagrant/python.sh
+++ vagrant/python.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "Updating box ..."
-sudo apt-get update > /dev/null
-sudo apt-get upgrade -y > /dev/null
+sudo apt-get -qq update
+sudo apt-get -y -qq upgrade
 
 if [ ! -x /usr/bin/git ]; then
     echo "Installing git ..."
-    sudo apt-get -y install git-core > /dev/null
+    sudo apt-get -y -qq install git-core
 fi
 
 if [ ! -x /usr/bin/python ]; then
     echo "Installing python ..."
-    sudo apt-get -y install python > /dev/null
+    sudo apt-get -y -qq install python python-dev
 fi

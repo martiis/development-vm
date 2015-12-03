a06bb06dad7a9af37eb922a208d0de96098adb0c
Martynas Sudintas <martynas.sudintas@ongr.io>
2015-12-03 11:59:44 +0200

    Reduced default swap size to 4GB

--- vagrant/python.sh
+++ vagrant/python.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Updating box ..."
+echo " [Shell script] Updating box ..."
 sudo apt-get -qq update
 sudo apt-get -y -qq upgrade
 
 if [ ! -x /usr/bin/git ]; then
-    echo "Installing git ..."
+    echo " [Shell script] Installing git ..."
     sudo apt-get -y -qq install git-core
 fi
 
 if [ ! -x /usr/bin/python ]; then
-    echo "Installing python ..."
+    echo " [Shell script] Installing python ..."
     sudo apt-get -y -qq install python python-dev
 fi
--- vagrant/swap.sh
+++ vagrant/swap.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
 # size of swapfile in megabytes
-swapsize=8000
+swapsize=4096
 
 # does the swap file already exist?
 grep -q "swapfile" /etc/fstab
 
 # if not then create it
 if [ $? -ne 0 ]; then
-  echo 'swapfile not found. Adding swapfile.'
+  echo ' [Swap] Swapfile not found. Adding swapfile...'
   fallocate -l ${swapsize}M /swapfile
   chmod 600 /swapfile
   mkswap /swapfile
   swapon /swapfile
   echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+  echo ' [Swap] Done'
 else
-  echo 'swapfile found. No changes made.'
+  echo ' [Swap] Swapfile found, no changes made.'
 fi
 
 # output results to terminal
